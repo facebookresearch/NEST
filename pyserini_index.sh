ef9c297f010f15480ea82ab0fe8f402786b16898
alexlimh <alexlimh23@gmail.com>
2024-07-19 07:13:36 -0400

    remove additional files

--- pyserini_index.sh
+++
@@ -1,7 +0,0 @@
-python -m pyserini.index.lucene \
-  --collection JsonCollection \
-  --input collections/enwiki-dec2021 \
-  --index indexes/enwiki-dec2021 \
-  --generator DefaultLuceneDocumentGenerator \
-  --threads 32 \
-  --optimize
